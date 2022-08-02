eb3645784697c0ae3df511b6b67a14155bc72469
wwartick <willwartick@gmail.com>
2022-08-02 16:24:52 -0700

    added prompts for each character choice

--- Develop/script.js
+++ Develop/script.js
@@ -17,17 +17,53 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 var lowercaseCheck = function(){
-  var lowerPrompt = window.prompt('Would you like lower case letters in your pass word? Y for YES or N for NO.');
-    if (lowerPrompt === "y" ||lowerPrompt === "yes" || lowerPrompt === "Y" ||lowerPrompt === "YES")
+  var lowerPrompt = window.prompt('Would you like lower case letters in your password? Y for YES or N for NO.');
+      if (lowerPrompt === "y" ||lowerPrompt === "yes" || lowerPrompt === "Y" ||lowerPrompt === "YES"){
       uppercaseCheck();
       /* array shit here*/
-      if (lowerPrompt === "n" ||lowerPrompt === "no" || lowerPrompt === "N" ||lowerPrompt === "NO")
+    } if (lowerPrompt === "n" ||lowerPrompt === "no" || lowerPrompt === "N" ||lowerPrompt === "NO"){
       uppercaseCheck();
-    else {
+     }else {
         window.alert("You need to provide a valid answer! Please try again");
         return lowercaseCheck();
   }
-
-
 }
-as
+
+var uppercaseCheck = function(){
+  var upperPrompt = window.prompt('Would you like upper case letters in your password? Y for YES or N for NO.');
+      if (upperPrompt === "y" ||upperPrompt === "yes" || upperPrompt === "Y" ||upperPrompt === "YES"){
+      numberCheck();
+      /* array shit here*/
+      }if (upperPrompt === "n" ||upperPrompt === "no" || upperPrompt === "N" ||upperPrompt === "NO"){
+      numberCheck();
+      }else {
+        window.alert("You need to provide a valid answer! Please try again");
+        return uppercaseCheck();
+  }
+}
+
+var numberCheck = function(){
+  var numberPrompt = window.prompt('Would you like numbers in your password? Y for YES or N for NO.');
+     if (numberPrompt === "y" ||numberPrompt === "yes" || numberPrompt === "Y" ||numberPrompt === "YES"){
+      specialCheck();
+      /* array shit here*/
+      }if (numberPrompt === "n" ||numberPrompt === "no" || numberPrompt === "N" ||numberPrompt === "NO"){
+      specialCheck();
+      }else {
+        window.alert("You need to provide a valid answer! Please try again");
+        return numberCheck();
+  }
+}
+
+var specialCheck = function(){
+  var specialPrompt = window.prompt('Would you like special case characters in your password? Y for YES or N for NO.');
+    if (specialPrompt === "y" ||specialPrompt === "yes" || specialPrompt === "Y" ||specialPrompt === "YES"){
+    window.prompt('donezo');
+      /* array shit here*/
+      }if (specialPrompt === "n" ||specialPrompt === "no" || specialPrompt === "N" ||specialPrompt === "NO"){
+      window.prompt('donezo');
+      }else {
+        window.alert("You need to provide a valid answer! Please try again");
+        return specialCheck();
+  }
+}
