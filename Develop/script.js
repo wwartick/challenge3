e104ab023d8347999cb4a591ba547e847b6abbe2
wwartick <willwartick@gmail.com>
2022-08-02 18:47:04 -0700

    final comments and touchups

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,21 @@
 // Assignment code here
-var randomNumber = Math.floor(Math.random() * 126) + 8;
-var passwordLength;
-
+/* array declarations for each character choice + options*/
 const lowerArray =['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 const upperArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 const numberArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-const specialArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+const specialArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", ">", "<"];
+
+/* variables used for final result */
 var holder = new Array();
 var finalHolder = new Array();
+var passwordLength;
 
-
-
+/* starting function - checks for length and makes sure it is a numeric*/
 var lengthCheck = function(){
   holder = [];
   var lengthPrompt = window.prompt('Select a password length between 8 and 128 characters');
     if (lengthPrompt >= 8 && lengthPrompt <= 128){
      passwordLength = lengthPrompt;
-      console.log(passwordLength);
       lowercaseCheck();
       return;
 
@@ -26,6 +25,7 @@ var lengthCheck = function(){
     }
 }
 
+/*lower case prompt - push command adds the lower case array to a new "holder" array if user inputs yes */
 var lowercaseCheck = function(){
   var lowerPrompt = window.prompt('Would you like lower case letters in your password? Y for YES or N for NO.');
     if (lowerPrompt === "y" ||lowerPrompt === "yes" || lowerPrompt === "Y" ||lowerPrompt === "YES"){
@@ -42,7 +42,7 @@ var lowercaseCheck = function(){
         return lowercaseCheck();
   }
 }
-
+/*upper case prompt */
 var uppercaseCheck = function(){
   var upperPrompt = window.prompt('Would you like upper case letters in your password? Y for YES or N for NO.');
     if (upperPrompt === "y" ||upperPrompt === "yes" || upperPrompt === "Y" ||upperPrompt === "YES"){
@@ -59,7 +59,7 @@ var uppercaseCheck = function(){
         return uppercaseCheck();
   }
 }
-
+/*number prompt */
 var numberCheck = function(){
   var numberPrompt = window.prompt('Would you like numbers in your password? Y for YES or N for NO.');
     if (numberPrompt === "y" ||numberPrompt === "yes" || numberPrompt === "Y" ||numberPrompt === "YES"){
@@ -76,16 +76,16 @@ var numberCheck = function(){
         return numberCheck();
   }
 }
-
+/*special character prompt */
 var specialCheck = function(){
   var specialPrompt = window.prompt('Would you like special case characters in your password? Y for YES or N for NO.');
     if (specialPrompt === "y" ||specialPrompt === "yes" || specialPrompt === "Y" ||specialPrompt === "YES"){
       holder.push(specialArray);
      randomizer();
      return;
-
+    /*checks to make sure at least one prompt was said yes to */
     }if ((specialPrompt === "n" ||specialPrompt === "no" || specialPrompt === "N" ||specialPrompt === "NO") && holder.length == 0 ){
-      window.alert("You must select at least on option! Please try again");
+      window.alert("You must select at least one option! Please try again");
       lowercaseCheck();
     }else if (specialPrompt === "n" ||specialPrompt === "no" || specialPrompt === "N" ||specialPrompt === "NO"){
       randomizer();
@@ -97,7 +97,7 @@ var specialCheck = function(){
   }
 }
 
-
+/*picks a random item from a randomly picked array, and assigns the value to a final holder array */
 var randomizer = function(){
   finalHolder = []
   for(let i = 0; i < passwordLength; i++){
@@ -106,6 +106,7 @@ var randomizer = function(){
   }
 }
 
+/*converts the final array to string. the .join function removes all spaces and commas for the final password */
 var generatePassword = function() {
   var finalPassword = finalHolder.join("");
   return finalPassword;
